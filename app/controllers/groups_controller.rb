b86af93868a4f62e22c4891cac39de1fe39b3237
weifan <p988744@gmail.com>
2015-10-15 14:37:06 +0800

    first commit

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,51 @@
+class GroupsController < ApplicationController
+	def index
+		flash[:notice] = "Good morning!"
+		@groups = Group.all
+	end
+
+	def show
+		@group = Group.find(params[:id])
+	end
+
+	def new
+		@group = Group.new	
+	end
+
+	def edit
+		@group = Group.find(params[:id])
+	end
+
+	def create
+		@group = Group.create(group_params)
+	
+	   if @group.save
+	     redirect_to groups_path
+	   else
+	     render :new
+	   end
+	end
+
+	def update
+		@grooup = Group.find(params[:id])
+
+		if @group.update(group_params)
+			redirect_to groups_path, notice: "修改討論版成功"
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		
+	end
+
+	private
+
+	def group_params 
+	  params.require(:group).permit(:title, :description)
+	end
+
+end
+
+
